ee18a180a9b29c6248eb1002b6481a3e8335c7a8
Tay Kai Yang <kaiyang.tay@amadeus.com>
2015-12-23 17:50:17 +0800

    Commit initial dump

---
+++ javascript/angularjs2-quickstart/web/app/boot.js
@@ -0,0 +1,8 @@
+/**
+ * @author kaiyang.tay
+ */
+(function(app) {
+	document.addEventListener('DOMContentLoaded', function() {
+		ng.platform.browser.bootstrap(app.AppComponent);
+	});
+})(window.app || (window.app = {}));
